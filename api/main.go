f6f6089317022d83f46d93b84f91adadfb524989
Kevin Kobori <kevinyujikobori@gmail.com>
2021-07-06 23:34:53 -0300

    portguese version, cant setcookie on browser

---
+++ api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"api/src/config"
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// func init() {
+// 	secretKey := make([]byte, 64)
+
+// 	if _, err := rand.Read(secretKey); err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	stringBase64 := base64.StdEncoding.EncodeToString(secretKey)
+// 	fmt.Println(stringBase64)
+// }
+
+func main() {
+	config.Carregar()
+	r := router.Gerar()
+
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
+}
